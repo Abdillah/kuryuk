28f975770f7870427e4cb0f839ba03072acb0a71
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2020-05-30 13:31:25 +0700

    Add transaction_category model

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,33 @@
 use crate::schema::transactions;
 use crate::schema::categories;
+use crate::schema::transaction_category;
 
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub enum TransactionType {
     Expense,
     Income,
+}
+
+impl std::convert::From<String> for TransactionType {
+    fn from(s: String) -> Self {
+        match s.to_lowercase().as_str() {
+            "expense" => Self::Expense,
+            "income" => Self::Income,
+            _ => {
+                eprintln!("ERR: TransactionType {} not supported, fallback to Expense", s);
+                Self::Expense
+            },
+        }
+    }
+}
+
+impl std::convert::From<TransactionType> for String {
+    fn from(s: TransactionType) -> String {
+        match s {
+            TransactionType::Expense => "Expense".to_string(),
+            TransactionType::Income => "Income".to_string(),
+        }
+    }
 }
 
 impl diesel::Expression for TransactionType {
@@ -13,10 +36,28 @@ impl diesel::Expression for TransactionType {
 
 impl std::fmt::Display for TransactionType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(match self {
-            Self::Expense => "TransactionType::Expense",
-            Self::Income => "TransactionType::Income",
-        })
+        f.write_str(String::from(*self).as_str())
+    }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct TransactionCreateRequest {
+    pub trx_id: String,
+    pub title: String,
+    pub description: String,
+    pub category_id: Option<i32>,
+}
+
+impl std::convert::Into<Transaction> for TransactionCreateRequest {
+    fn into(self) -> Transaction {
+        Transaction {
+            id: None,
+            trx_id: self.trx_id,
+            title: self.title,
+            description: self.description,
+            created_at: None,
+            updated_at: None,
+        }
     }
 }
 
@@ -37,8 +78,16 @@ pub struct Transaction {
 #[table_name="categories"]
 pub struct Category {
     pub id: Option<i32>,
+    pub parent_id: Option<i32>,
+    #[diesel(deserialize_as = "String")]
     pub type_: TransactionType,
     pub icon: String,
     pub title: String,
-    pub description: String,
+}
+
+#[derive(Debug, diesel::Insertable, diesel::Queryable, serde::Serialize, serde::Deserialize)]
+#[table_name="transaction_category"]
+pub struct TransactionCategory {
+    pub transaction_id: i32,
+    pub category_id: i32,
 }
